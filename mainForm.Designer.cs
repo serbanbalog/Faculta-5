9ee35b4181af9aa28b3dd6e8083159ae1dcd5800
Serban <iustinel78@gmail.com>
2022-12-08 09:45:51 +0200

    Final 08.12

--- mainForm.Designer.cs
+++ mainForm.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.InsertButton = new System.Windows.Forms.Button();
             this.refreshButton = new System.Windows.Forms.Button();
+            this.newGameButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // InsertButton
@@ -52,17 +54,37 @@
             this.refreshButton.UseVisualStyleBackColor = true;
             this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
             // 
+            // newGameButton
+            // 
+            this.newGameButton.Location = new System.Drawing.Point(212, 12);
+            this.newGameButton.Name = "newGameButton";
+            this.newGameButton.Size = new System.Drawing.Size(94, 29);
+            this.newGameButton.TabIndex = 2;
+            this.newGameButton.Text = "New Game";
+            this.newGameButton.UseVisualStyleBackColor = true;
+            this.newGameButton.Click += new System.EventHandler(this.newGameButton_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(193, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 3;
+            // 
             // mainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1227, 692);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.newGameButton);
             this.Controls.Add(this.refreshButton);
             this.Controls.Add(this.InsertButton);
             this.Name = "mainForm";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.mainForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +92,7 @@
 
         private Button InsertButton;
         private Button refreshButton;
+        private Button newGameButton;
+        private TextBox textBox1;
     }
 }
--- mainForm.cs
+++ mainForm.cs
@@ -5,6 +5,8 @@ namespace Faculta_5
     public partial class mainForm : Form
     {
         ListBox listBox;
+        int ncs;
+        Random rnd = new Random();
         public mainForm()
         {
             InitializeComponent();
@@ -24,7 +26,7 @@ namespace Faculta_5
             listBox.Width = 90;
             listBox.Height = 500;
             listBox.Top = 50;
-            listBox.Left = 10;
+            listBox.Left = 12;
             listBox.Parent = this;
             listBox.Font = new Font("Arial", 20, FontStyle.Regular);
         }
@@ -37,7 +39,125 @@ namespace Faculta_5
         public void refresh()
         {
             listBox.Items.Add(Engine.x.ToString());
+        }
 
+        public void newGame()
+        {
+            ncs = generateNumber4();
+            textBox1.Text = ncs.ToString();
+        }
+        public int generateNumber1()
+        {
+            bool ok;
+            int x;
+            do
+            {
+                ok = true;
+                x = rnd.Next(1000, 10000);
+                int c1 = x % 10;
+                int c2 = (x / 10) % 10;
+                int c3 = (x / 100) % 10;
+                int c4 = (x / 1000) % 10;
+                if (c1 == c2 || c1 == c3 || c1 == c4 || c2 == c3 || c2 == c4 || c3 == c4)
+                    ok = false;
+            } while (!ok);
+            return x;
+        }
+        public int generateNumber2()
+        {
+            int x;
+            int c1 = rnd.Next(1,10);
+            int c2, c3, c4;
+            do
+            {
+                c2 = rnd.Next(10);
+                
+            }while(c1==c2);
+            bool ok;
+            do
+            {
+                ok = true;
+                c3 = rnd.Next(10);
+                if (c1 == c3 || c2 == c3)
+                    ok = false;
+
+            } while (!ok);
+            do
+            {
+                ok = true;
+                c4 = rnd.Next(10);
+                if ( c1 == c4 || c2 == c4 || c3 == c4)
+                    ok = false;
+
+            } while (!ok);
+            return c1 * 1000 + c2 * 100 + c3 * 10 + c4;
+
+        }
+
+        public int generateNumber3()
+        {
+            int[] v = new int[10];
+            int x;
+            do
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    v[i] = i + 1;
+                }
+                for (int i = 0; i < v.Length; i++)
+                {
+                    int t = rnd.Next(10);
+                    int aux = v[i];
+                    v[i] = v[t];
+                    v[t] = aux;
+                }
+                x = 0;
+                for (int i = 0; i < v.Length; i++)
+                {
+                    switch (v[i])
+                    {
+                        case 1:
+                            x += i * 1000;
+                            break;
+                        case 2:
+                            x += i * 100;
+                            break;
+                        case 3:
+                            x += i * 10;
+                            break;
+                        case 4:
+                            x += i;
+                            break;
+                    }
+                }
+            } while (x < 1000);
+            return x;
+        }
+
+        public int generateNumber4()
+        {
+            List<int> intregi = new List<int>();
+            for(int i =0; i < 10; i++)
+            {
+                intregi.Add(i);
+            }
+            int t = rnd.Next(1, 10);
+            int x = intregi[t] * 1000;
+            intregi.RemoveAt(t);
+            t = rnd.Next(intregi.Count);
+            x += intregi[t]*100;
+            intregi.RemoveAt(t);
+            t = rnd.Next(intregi.Count);
+            x += intregi[t] * 10;
+            intregi.RemoveAt(t);
+            t = rnd.Next(intregi.Count);
+            x += intregi[t];
+
+            return x;
+        }
+        private void newGameButton_Click(object sender, EventArgs e)
+        {
+            newGame();
         }
     }
 }
